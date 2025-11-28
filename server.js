0c13300a7570da90718665f669e245a46d3d06e4
gabrieletodaroo9 <gabrieletodaroo9@gmail.com>
2025-11-28 11:39:34 +0100

    create middleware for the internal error of the server

--- server.js
+++ server.js
@@ -10,8 +10,14 @@ app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`)
 })
 
-app.get('/', (req, res) => {
+app.get('/', (req, resp) => {
     res.send("This is the entrypoint of the server")
 })
 
 app.use('/api/posts', postsRouter)
+
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).json({ err: err.message })
+})
+
